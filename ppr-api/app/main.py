099c7738986941352dcc11bc6a464381f17864e2
Walter Moar <Walter.Moar@gov.bc.ca>
2019-10-31 11:01:33 -0700

    Hello world backend PoC. #5

---
+++ ppr-api/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "https://ppr-dev.pathfinder.gov.bc.ca"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
