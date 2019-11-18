8f89fc945594079393370ed44dc79fd9c95c4d51
Justin Guertin <justin.guertin@gmail.com>
2019-11-18 09:10:04 -0800

    2 changes to support development in PyCharm and debugging (#21)
    - Added lines to the main app to run as a python script so that it can be run in the IDE
    - Add an ignore line for PyCharm project directory

--- ppr-api/app/main.py
+++ ppr-api/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -16,6 +17,11 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
