20d358f128e4f348126540ff72b5a17b0c6ee756
Justin Guertin <justin.guertin@gmail.com>
2019-11-26 10:25:48 -0800

    Integrate Sentry with the IMS API

--- ims-api/src/main/java/ca/bc/gov/registries/ppr/App.java
+++ ims-api/src/main/java/ca/bc/gov/registries/ppr/App.java
@@ -1,11 +1,26 @@
 package ca.bc.gov.registries.ppr;
 
+import io.sentry.spring.SentryExceptionResolver;
+import io.sentry.spring.SentryServletContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public HandlerExceptionResolver sentryExceptionResolver() {
+        return new SentryExceptionResolver();
+    }
+
+    @Bean
+    public ServletContextInitializer sentryServletContextInitializer() {
+        return new SentryServletContextInitializer();
     }
 }
