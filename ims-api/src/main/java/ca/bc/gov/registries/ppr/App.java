929eb20074217f027b0903ba8082e0589a2edd95
Justin Guertin <justin.guertin@gmail.com>
2019-11-26 14:11:20 -0800

    Integrate Jaeger with the IMS API

--- ims-api/src/main/java/ca/bc/gov/registries/ppr/App.java
+++ ims-api/src/main/java/ca/bc/gov/registries/ppr/App.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.registries.ppr;
 
+import io.jaegertracing.Configuration;
+import io.opentracing.Tracer;
 import io.sentry.spring.SentryExceptionResolver;
 import io.sentry.spring.SentryServletContextInitializer;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,8 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
 public class App {
+    private static final String APP_NAME = "ims-api";
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
@@ -22,5 +26,10 @@ public class App {
     @Bean
     public ServletContextInitializer sentryServletContextInitializer() {
         return new SentryServletContextInitializer();
+    }
+
+    @Bean
+    public Tracer tracer() {
+        return Configuration.fromEnv(APP_NAME).getTracer();
     }
 }
