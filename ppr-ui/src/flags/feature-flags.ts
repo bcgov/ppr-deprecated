483a801dbdc8bca1e56b2964037f3748bea8bbeb
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-12-09 09:14:19 -0800

    Add docs and type

--- ppr-ui/src/flags/feature-flags.ts
+++ ppr-ui/src/flags/feature-flags.ts
@@ -1,4 +1,6 @@
-import {toRefs, reactive} from "@vue/composition-api"
+import {toRefs, Ref, reactive} from "@vue/composition-api"
+import {Data} from "@vue/composition-api/dist/component"
+
 
 let _flags
 
@@ -9,8 +11,36 @@ export function initializeFeatureFlags(): void {
   })
 }
 
-// Return object. Ideally the composition API would export the Refs<> type
-export function useFeatureFlags(): any {
+
+/*
+export function useFeatureFlags() {
+  return toRefs(_flags)
+}
+
+If useFeatureFlags() does not have return type the linter produces the warning:
+
+warning: Missing return type on function (@typescript-eslint/explicit-function-return-type)
+
+With the correct return type but without Refs<> being defined any where the definition:
+
+export function useFeatureFlags(): Refs<Data> {
+  return toRefs(_flags)
+}
+Produces the error:
+
+TypeScript diagnostics (customize using `[jest-config].globals.ts-jest.diagnostics` option):
+error TS2304: Cannot find name 'Refs'.
+
+To resolve we can copy from  "@vue/composition-api/dist/reactivity/ref" the Refs<> type.
+Ideally the composition API would export the Refs<> type.
+
+*/
+
+export declare type Refs<Data> = {
+  [K in keyof Data]: Data[K] extends Ref<infer V> ? Ref<V> : Ref<Data[K]>;
+}
+
+export function useFeatureFlags(): Refs<Data> {
   return toRefs(_flags)
 }
 
