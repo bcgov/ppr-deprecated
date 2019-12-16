f065f57b0835121a4099c36aeb53fba0bafdcf00
Walter Moar <Walter.Moar@gov.bc.ca>
2019-12-16 10:44:14 -0800

    Miscellaneous cleanup before it goes too far. #80 (#75)

--- ppr-api/src/main.py
+++ ppr-api/src/main.py
@@ -1,10 +1,10 @@
+import fastapi
+from starlette.middleware import cors
 import uvicorn
-from fastapi import FastAPI, Header, HTTPException
-from starlette.middleware.cors import CORSMiddleware
 
 from endpoints import healthcheck
 
-app = FastAPI()
+app = fastapi.FastAPI()
 app.include_router(healthcheck.router)
 
 origins = [
@@ -13,7 +13,7 @@ origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware,
+    cors.CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
@@ -30,16 +30,16 @@ async def root():
 # sample read header
 # sample return json structure
 @app.get("/auth/{user_name}", status_code=201)
-async def read_item(user_name: str, user_agent: str = Header(None)):
-    my_list = ['justin', 'walter', 'bryan']
+async def read_item(user_name: str, user_agent: str = fastapi.Header(None)):
+    my_list = ["justin", "walter", "bryan"]
     print(my_list)
     print(user_name)
     if user_name in my_list:
         return {"user_name": user_name, "user_agent": user_agent}
-    raise HTTPException(status_code=404, detail="Invalid user name")
+    raise fastapi.HTTPException(status_code=404, detail="Invalid user name")
 
 
-# You should use uvicorn to run the app locally.  __main__ is provided to run
-# it in a debugger. See https://fastapi.tiangolo.com/tutorial/debugging/
+# You should use uvicorn to run the app locally.  __main__ is provided to run it in a debugger. See
+# https://fastapi.tiangolo.com/tutorial/debugging/
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
