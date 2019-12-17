2db069c499cf39b524d97f3d124195990a772ded
Justin Guertin <justin.guertin@gmail.com>
2019-12-17 15:55:05 -0800

    Remove the "Hello World" endpoints from the APIs

--- ppr-api/tests/unit/test_main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from main import root
-
-
-def test_root():
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(root())
-    loop.close()
-    assert {"message": "Hello World"} == result
