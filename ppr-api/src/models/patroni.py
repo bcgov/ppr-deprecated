f74162e3a92b11f4d9c49d0dbe24729225fd1d29
Walter Moar <Walter.Moar@gov.bc.ca>
2020-01-06 16:52:55 -0800

    Add new ops endpoint for Patroni.

---
+++ ppr-api/src/models/patroni.py
@@ -0,0 +1,29 @@
+""" Set up SQL Alchemy to access the Patroni database. """
+
+import sqlalchemy
+import sqlalchemy.orm
+
+import config
+
+DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+    user=config.DB_USERNAME,
+    password=config.DB_PASSWORD,
+    host=config.DB_HOSTNAME_PATRONI,
+    port=config.DB_PORT,
+    name=config.DB_NAME
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URI)
+SessionLocal = sqlalchemy.orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session():
+    """
+    Returns a session using yield to facilitate using it as a dependency in FastAPI.  See
+    https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
