7c15dd1978fe28a1da5025b8be34d298085946d6
Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>
2020-01-20 09:06:37 -0800

    Add comman

--- ppr-api/src/main.py
+++ ppr-api/src/main.py
@@ -13,7 +13,7 @@ app.include_router(api.router)
 
 origins = [
     "http://localhost:8080",
-    "https://ppr-dev.pathfinder.gov.bc.ca"
+    "https://ppr-dev.pathfinder.gov.bc.ca",
     "https://dev.bcregistry.ca"
 ]
 
