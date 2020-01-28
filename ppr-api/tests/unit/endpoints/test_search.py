094bb6b85af1f7b64bb191d0fdc9bd79f972b9c6
Justin Guertin <justin.guertin@gmail.com>
2020-01-28 15:33:05 -0800

    Add additional tests for the api endpoint that provides search results

--- ppr-api/tests/unit/endpoints/test_search.py
+++ ppr-api/tests/unit/endpoints/test_search.py
@@ -46,10 +46,8 @@ def test_read_search_results_is_empty():
 
 
 def test_read_search_results_has_exact_match():
-    stub_fin_stmt = stub_financing_statement_event('123456A')
-    search_record = models.search.Search(results=[models.search.SearchResult(exact=True, selected=True,
-                                                                             registration_number='123456A',
-                                                                             financing_statement_event=stub_fin_stmt)])
+    stub_fin_stmt_event = stub_financing_statement_event('123456A')
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event, True)])
     repo = MockSearchRepository(search_record)
 
     results = endpoints.search.read_search_results(27, repo)
@@ -58,10 +56,8 @@ def test_read_search_results_has_exact_match():
 
 
 def test_read_search_results_has_inexact_match():
-    stub_fin_stmt = stub_financing_statement_event('123456A')
-    search_record = models.search.Search(results=[models.search.SearchResult(exact=False, selected=True,
-                                                                             registration_number='123456A',
-                                                                             financing_statement_event=stub_fin_stmt)])
+    stub_fin_stmt_event = stub_financing_statement_event('123456A')
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event, False)])
     repo = MockSearchRepository(search_record)
 
     results = endpoints.search.read_search_results(27, repo)
@@ -69,13 +65,66 @@ def test_read_search_results_has_inexact_match():
     assert results[0].type == 'SIMILAR'
 
 
-def stub_financing_statement_event(reg_number: str):
+def test_read_search_results_provides_event_base_registration_number():
+    stub_fin_stmt_event = stub_financing_statement_event('123456A', '765432B')
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event)])
+    repo = MockSearchRepository(search_record)
+
+    results = endpoints.search.read_search_results(27, repo)
+
+    assert results[0].financingStatement.baseRegistrationNumber == '765432B'
+
+
+def test_read_search_results_provides_event_registration_date():
+    stub_fin_stmt_event = stub_financing_statement_event('123456A')
+    stub_fin_stmt_event.registration_date = datetime.datetime.now() - datetime.timedelta(hours=1)
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event)])
+    repo = MockSearchRepository(search_record)
+
+    results = endpoints.search.read_search_results(27, repo)
+
+    assert results[0].financingStatement.registrationDateTime == stub_fin_stmt_event.registration_date
+
+
+def test_read_search_results_provides_event_document_number():
+    stub_fin_stmt_event = stub_financing_statement_event('123456A')
+    stub_fin_stmt_event.document_number = 'Z9876543'
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event)])
+    repo = MockSearchRepository(search_record)
+
+    results = endpoints.search.read_search_results(27, repo)
+
+    assert results[0].financingStatement.documentId == 'Z9876543'
+
+
+def test_read_search_results_provides_financing_statement_expiry_date():
+    stub_fin_stmt_event = stub_financing_statement_event('123456A')
+    stub_fin_stmt_event.base_registration.expiry_date = datetime.date.today() + datetime.timedelta(days=27)
+    search_record = models.search.Search(results=[stub_search_result(stub_fin_stmt_event)])
+    repo = MockSearchRepository(search_record)
+
+    results = endpoints.search.read_search_results(27, repo)
+
+    assert results[0].financingStatement.expiryDate == stub_fin_stmt_event.base_registration.expiry_date
+
+
+def stub_financing_statement_event(reg_number: str, base_reg_number: str = None):
+    if not base_reg_number:
+        base_reg_number = reg_number
+
     return models.financing_statement.FinancingStatementEvent(
-        registration_number=reg_number, base_registration_number=reg_number, document_number='A1234567',
+        registration_number=reg_number, base_registration_number=base_reg_number, document_number='A1234567',
         registration_date=datetime.datetime.now(), base_registration=models.financing_statement.FinancingStatement(
-            registration_number='123456A', registration_type_code='SA'
+            registration_number=base_reg_number, registration_type_code='SA'
         )
     )
+
+
+def stub_search_result(registration: models.financing_statement.FinancingStatementEvent,
+                       exact: bool = True, selected: bool = True):
+    return models.search.SearchResult(exact=exact, selected=selected,
+                                      registration_number=registration.registration_number,
+                                      financing_statement_event=registration)
 
 
 class MockSearchRepository:
