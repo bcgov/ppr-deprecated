277bbed6482b59319aed224f6bd970a874cf4cf3
Justin Guertin <justin.guertin@gmail.com>
2020-01-31 16:48:14 -0800

    Add an authentication module which calls the Auth API with the provided user token

--- ppr-api/src/config.py
+++ ppr-api/src/config.py
@@ -12,6 +12,8 @@ import dotenv
 
 dotenv.load_dotenv()
 
+AUTH_API_URL = os.getenv("PPR_API_AUTH_API_URL")
+
 CORS_ORIGINS = os.getenv('PPR_API_ALLOWED_ORIGINS', 'http://localhost:8080 http://localhost:8081').split()
 
 DB_HOSTNAME = os.getenv('PPR_API_DB_HOSTNAME')
