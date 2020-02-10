fbc4a295a55628ede3c14d02f4cb4917fb68eae1
Justin Guertin <justin.guertin@gmail.com>
2020-02-10 11:45:44 -0800

    Remove the IMS API from the PPR system

--- ims-api/src/main/java/ca/bc/gov/registries/ppr/search/ISearchService.java
+++
@@ -1,7 +0,0 @@
-package ca.bc.gov.registries.ppr.search;
-
-import java.util.List;
-
-public interface ISearchService {
-    List<VehicleSummarySearchResult> findFinancialStatementsBySerial(String serial) throws Exception;
-}
