ec196ab0fe42187ad0978ecf4134f2e6dc96d335
Bryan Gilbert <bryan.gilbert@memsharp.com>
2020-02-14 15:41:36 -0800

    Redirect back to PPR on login and logout
    Auth team has updated the common header with two new properties to help apps redirect back to their apps.

---
+++ ppr-ui/src/layouts/LayoutHeader.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <sbc-header
+    redirect-on-login-success="encoded"
+    redirect-on-logout="encode"
+  />
+</template>
+
+<script lang="ts">
+import { createComponent, inject } from '@vue/composition-api'
+import SbcHeader from 'sbc-common-components/src/components/SbcHeader.vue'
+
+export default createComponent({
+  components: {
+    SbcHeader
+  },
+  setup() {
+    const oUrl = inject('originUrl') as string
+    const encoded = encodeURIComponent(oUrl)
+    return {
+      encoded
+    }
+  }
+})
+
+</script>
