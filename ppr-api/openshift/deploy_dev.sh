9df95272d10ddd012f5ab48c98c57f0aac74b372
Justin Guertin <justin.guertin@gmail.com>
2020-02-18 12:12:18 -0800

    Change the deployment scripts and helper commands to use a parameter file rather than inline `-p` parameters

--- ppr-api/openshift/deploy_dev.sh
+++ ppr-api/openshift/deploy_dev.sh
@@ -6,9 +6,4 @@ export NAME_PLATE=zwmtib
 export ENVIRONMENT=dev
 export SOURCE_TAG=latest
 
-export AUTH_API_URL=https://auth-api-dev.pathfinder.gov.bc.ca/api/v1
-export CORS_ORIGINS="http://localhost:8080 https://dev.bcregistry.ca https://ppr-dev.pathfinder.gov.bc.ca"
-export PAY_API_URL=https://pay-api-dev.pathfinder.gov.bc.ca/api/v1
-export ROUTE_URL=ppr-api-dev.pathfinder.gov.bc.ca
-
 ./deploy_ppr_api.sh
