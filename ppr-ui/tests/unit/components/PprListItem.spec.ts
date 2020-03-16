6e6dbe6fc2e657703dffd1122ab430d242282098
Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>
2020-03-16 16:16:34 -0700

    Issue #769 PPR List item (#770)
    * Issue #769 PPR List item
    * add tests for when editing is false

---
+++ ppr-ui/tests/unit/components/PprListItem.spec.ts
@@ -0,0 +1,77 @@
+import Vue from 'vue'
+import VueCompositionApi, { ref } from '@vue/composition-api'
+import { mount, Wrapper } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+
+import PprListItem from '@/components/PprListItem.vue'
+
+Vue.use(Vuetify)
+Vue.use(VueCompositionApi)
+const vuetify = new Vuetify()
+
+describe('PprListItem.vue', (): void => {
+  describe(':props', (): void => {
+
+    // General form tests.
+
+    it(':editing - false, header does not exist', (): void => {
+      const properties = ref({ editing: false, listLength: 1, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('div[data-test-id="ListItem.header.title"]').exists()).toBeFalsy()
+    })
+
+    it(':editing - false, list of 2 does not have remove ', (): void => {
+      const properties = ref({ editing: false, listLength: 2, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('button[data-test-id="ListItem.button.remove"]').exists()).toBeFalsy()
+    })
+
+
+    it(':editing - true, header exist', (): void => {
+      const properties = ref({ editing: true, listLength: 1, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('div[data-test-id="ListItem.header.title"]').exists()).toBeTruthy()
+    })
+
+    it(':editing - true, list of 0 does not have remove ', (): void => {
+      const properties = ref({ editing: true, listLength: 0, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('button[data-test-id="ListItem.button.remove"]').exists()).toBeFalsy()
+    })
+
+    it(':editing - true, list of 1 does not have remove ', (): void => {
+      const properties = ref({ editing: true, listLength: 1, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('button[data-test-id="ListItem.button.remove"]').exists()).toBeFalsy()
+    })
+
+    it(':editing - true, list of 2 does have remove ', (): void => {
+      const properties = ref({ editing: true, listLength: 2, index: 0 })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      expect(wrapper.find('button[data-test-id="ListItem.button.remove"]').exists()).toBeTruthy()
+    })
+
+
+  })
+  describe('@events', (): void => {
+    it('@input - business name change should be emitted', async (): Promise<void> => {
+      const expected = 2
+      const properties = ref({ editing: true, listLength: 5, index: expected })
+      const wrapper: Wrapper<Vue> = mount(PprListItem, { propsData: properties.value, vuetify })
+
+      const button = wrapper.get('[data-test-id="ListItem.button.remove"]')
+      button.trigger('click')
+      await Vue.nextTick()
+
+      const emitted = wrapper.emitted('remove').slice(-1)[0][0]
+      expect(emitted).toBe(expected)
+    })
+
+  })
+})
