2dd18945a97d5789c3a7f3b2ed9c47251546ae6e
Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>
2020-03-27 04:17:43 -0700

    Start a prototype branch (#873)
    * Start a prototype branch
    * Proto - Suppress code coverage on Proto

---
+++ ppr-ui/src/proto/Proto.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-container class="view-container">
+      <router-link to="home">
+        Home
+      </router-link>
+    </v-container>
+    <v-container class="view-container">
+      <article>
+        <header>
+          <h1>Personal Property Registry - a prototype</h1>
+        </header>
+
+        <div class="page-content">
+          <div class="page-content__main">
+            <p>
+              This will be a PPR prototype page
+            </p>
+          </div>
+        </div>
+      </article>
+    </v-container>
+  </div>
+</template>
