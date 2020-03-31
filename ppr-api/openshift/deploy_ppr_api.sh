b7e7e059c4d6d5d36f92e0504ec3521cc67c573b
Justin Guertin <justin.guertin@gmail.com>
2020-03-31 15:19:09 -0700

    Update the OpenShift deployment configuration and scripts to include secrets. To use the scripts, some secrets will need to be set as environment variables.

--- ppr-api/openshift/deploy_ppr_api.sh
+++ ppr-api/openshift/deploy_ppr_api.sh
@@ -5,6 +5,9 @@
 : ${NAME_PLATE:?"NAME_PLATE environment variable is required"}
 : ${ENVIRONMENT:?"ENVIRONMENT environment variable is required"}
 : ${SOURCE_TAG:?"SOURCE_TAG environment variable is required"}
+: ${DB_PASSWORD:?"DB_PASSWORD environment variable is required"}
+: ${DB_USERNAME:?"DB_USERNAME environment variable is required"}
+: ${SENTRY_DSN:?"SENTRY_DSN environment variable is required"}
 
 env_namespace=${NAME_PLATE}-$ENVIRONMENT
 tools_namespace=${NAME_PLATE}-tools
@@ -13,7 +16,7 @@ source_tag=$SOURCE_TAG
 target_tag=$ENVIRONMENT
 
 # Apply the current configuration to the environment. There is no rollout trigger on configuration changes.
-oc process -f ppr-api-dc.yaml --param-file=$param_file | oc apply -f - -n $env_namespace
+oc process -f ppr-api-dc.yaml --param-file=$param_file -p DB_PASSWORD=$DB_PASSWORD -p DB_USERNAME=$DB_USERNAME -p SENTRY_DSN=$SENTRY_DSN | oc apply -f - -n $env_namespace
 
 # Tag the new image which triggers a rollout. In the future we may consider removing the trigger to explicitly run:
 #   `oc rollout latest dc/ppr-api -n $env_namespace`
