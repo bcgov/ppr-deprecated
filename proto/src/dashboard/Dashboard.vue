025f22c2312c7851900ad90f312eaed8bcb81c33
Bryan Gilbert <bryan.gilbert@memsharp.com>
2020-03-31 16:54:01 -0700

    draft tab -- good enough mock up of wireframe

---
+++ proto/src/dashboard/Dashboard.vue
@@ -0,0 +1,77 @@
+
+<template lang="pug">
+  v-tabs(
+    fixed-tabs,
+    background-color="white"
+    dark
+  )
+    v-tab Registerations
+    v-tab Searches
+    v-tab Client Codes
+    v-tab Drafts
+    v-tab-item
+      v-card
+        v-simple-table
+          tbody
+            tr(v-for="financingStatement in financingStatementsList")
+              td
+                financing-statement-condensed(:value="financingStatement")
+    v-tab-item
+      v-card
+        div This tab is a placeholder for searches this user has conducted in the past
+        div(style="padding-left: 1rem")
+          div {{ currentUser.name }} {{ currentUser.last }},
+          div {{ currentUser.company }},
+          div {{ currentUser.occupation }}
+    v-tab-item
+      v-card
+        party-codes
+    v-tab-item
+      v-card
+        drafts
+</template>
+<script lang="ts">
+  import { createComponent } from '@vue/composition-api'
+  import { useFinancingStatements } from '@/financing-statement/financing-statement-store'
+  import { useUsers } from '@/users/users'
+  import FinancingStatementCondensed from '@/financing-statement/FinancingStatementCondensed.vue'
+  import Drafts from '@/drafts/Drafts.vue'
+  import PartyCodes from '@/dashboard/PartyCodes.vue'
+
+  export default createComponent({
+    components: { Drafts, FinancingStatementCondensed, PartyCodes },
+    setup(_, { root }) {
+      const { financingStatementsList } = useFinancingStatements()
+      const { authenticated, currentUser} = useUsers()
+
+      return {authenticated, currentUser, financingStatementsList}
+    }
+  })
+
+</script>
+<style lang="scss" scoped>
+  .v-tab {
+    background-color: #0d47a1 !important;
+    margin-right: 1rem;
+  }
+  .v-tab--active {
+    background-color: #fefefe !important;
+    color: #0d47a1 !important;
+  }
+  .v-data-table {
+    th {
+      background-color: #38598A;
+      color: #F8F9FA  !important;
+      font-weight: bolder;
+      font-size: 1.5rem;
+    }
+
+    th, td {
+      border: 0.5px dotted darkgrey;
+      padding-bottom: 2rem;
+    }
+    tbody tr:nth-of-type(even) {
+      background-color: rgba(0, 0, 0, .05);
+    }
+  }
+</style>
---
+++ proto/src/dashboard/PartyCodes.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    party-code(v-for="party in partyList", :value="party", class="party-code")
+
+</template>
+<script lang="ts">
+  import { createComponent } from '@vue/composition-api'
+  import { usePartyCodes } from '../party-code/party-code-model'
+  import PartyCode from '@/party-code/PartyCode.vue'
+  export default createComponent({
+    components: { PartyCode },
+    setup(_, { root }) {
+      const { partyList } = usePartyCodes()
+
+      return {partyList}
+    }
+  })
+
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../assets/styles/theme.scss';
+  .party-code {
+    margin: 1rem 0;
+  }
+</style>
