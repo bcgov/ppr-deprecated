6949a09d5f964ba8d8cc6e629222a1af88a63e8d
Bryan Gilbert <bryan.gilbert@memsharp.com>
2020-03-31 16:54:01 -0700

    draft tab -- good enough mock up of wireframe

---
+++ proto/src/drafts/Draft.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  div
+    v-row
+      v-col(
+        :key="1",
+        cols="12",
+        sm="4",
+      )
+        v-card(
+          class="pa-2",
+          outlined,
+          tile
+        )
+          div Type: {{ draft.type }}
+          div Name: {{ draft.name }}
+      v-col(
+      :key="2",
+      cols="12",
+      sm="4",
+      )
+        v-card(
+        class="pa-2",
+        outlined,
+        tile
+        )
+          div Date Created: {{ draft.created }}
+          div Last Modified: {{ draft.last }}
+      v-col(
+      :key="3",
+      cols="12",
+      sm="4",
+      )
+        v-card(
+          class="pa-2",
+          outlined
+        )
+          v-btn(
+            icon,
+            color="red"
+              )
+            v-icon mdi-close-circle-outline
+            span Delete draft
+    v-row
+      v-col(
+      cols="12",
+      sm="12",
+      )
+        v-card(
+        class="pa-2",
+        outlined,
+        tile
+        )
+          v-btn Edit draft
+
+
+</template>
+<script lang="ts">
+  import { createComponent } from '@vue/composition-api'
+
+  export default createComponent({
+    components: {  },
+    props: {
+      draft: {
+        required: true,
+        type: Object
+      }
+    },
+    setup() {
+      return {}
+    }
+  })
+
+</script>
+
+<style lang="scss" scoped>
+  .v-btn--icon.v-size--default {
+    width: 10rem;
+  }
+  .v-card {
+    border: none !important;
+  }
+</style>
--- proto/src/views/Dashboard.vue
+++ proto/src/views/Dashboard.vue
@@ -4,55 +4,20 @@
       article
         header
           h1 Dashboard
-
         div(class="page-content")
           div(class="page-content__main")
-            p Placeholder page.
-        v-container
-          div(style="padding-left: 1rem")
-          div {{ currentUser.name }} {{ currentUser.last }},
-          div {{ currentUser.company }},
-          div {{ currentUser.occupation }}
-        v-simple-table
-          tbody
-            tr(v-for="financingStatement in financingStatementsList")
-              td
-                financing-statement-condensed(:value="financingStatement")
+            dash-board
 
 </template>
 <script lang="ts">
   import { createComponent } from '@vue/composition-api'
-  import { useFinancingStatements } from '@/financing-statement/financing-statement-store'
-  import { useUsers } from '@/users/users'
-  import FinancingStatementCondensed from '@/financing-statement/FinancingStatementCondensed.vue'
+  import DashBoard from '@/dashboard/Dashboard.vue'
 
   export default createComponent({
-    components: { FinancingStatementCondensed },
-    setup(_, { root }) {
-      const { financingStatementsList } = useFinancingStatements()
-      const { authenticated, currentUser} = useUsers()
-
-      return {authenticated, currentUser, financingStatementsList}
+    components: { DashBoard },
+    setup() {
+      return {}
     }
   })
 
 </script>
-<style lang="scss" scoped>
-
-  .v-data-table {
-    th {
-      background-color: #38598A;
-      color: #F8F9FA  !important;
-      font-weight: bolder;
-      font-size: 1.5rem;
-    }
-
-    th, td {
-      border: 0.5px dotted darkgrey;
-      padding-bottom: 2rem;
-    }
-    tbody tr:nth-of-type(even) {
-      background-color: rgba(0, 0, 0, .05);
-    }
-  }
-</style>
